b5b3be39f0b6328244753252eb6f114f03e06a89
satyamyadav <98868242+satyamy846@users.noreply.github.com>
2022-12-04 23:23:49 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,21 @@
+
+const uid = "abc2112";
+
+var FirstName = "Satyam";
+var lastName = "Yadav";
+var email = "satyadav941@gmail.com";
+var password = "1223456";
+var country = "India";
+var state = "Uttar Pradesh";
+var coursecount = 0;
+var isloggedinfromGoogle = false;
+
+console.log(`
+    UniqueID: ${uid},
+    FirstName: ${FirstName},
+    lastName : ${lastName},
+    email : ${email},
+    Password : ${password},
+    Country : ${country},
+    My state is : ${state},
+`)
